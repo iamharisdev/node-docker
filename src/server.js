bd43e67db47d1b6c79cf9fab63671f68b0caebee
M.Haris <admin@Apple-M1.local>
2026-09-08 08:25:35 +0500

    Test automatic deployments

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const server = http.createServer((req, res) => {
     "Content-Type": "text/plain"
   });
 
-  res.end(`${appName} is running! 🚀`);
+  res.end(`${appName} is running in live server! 🚀`);
 });
 
 async function start() {
